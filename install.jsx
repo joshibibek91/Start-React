827babaf7bf988913053a1726614a23808d820c9
Bibek <bibek.joshi91@gmail.com>
2024-04-05 13:41:26 +0545

    firstcommit

---
+++ install.jsx
@@ -0,0 +1,38 @@
+// How to install React
+
+1. Make folder and type cmd in path, open command prompt
+
+2. Create Vite
+
+    path> npm create vite@latest //or npm create vite@4
+
+    - Write project Name (e.g. home)
+    - Select framework as React
+    - Select variant as Javascript
+
+    //Some instructions come in written form
+
+3.Now Run
+
+    path>      cd home
+    path\home> npm install
+    path\home> code . //Go to VS-code
+
+
+    
+4. Execute server
+
+    In Terminal
+    path\home> npm run dev
+
+    //page opens of React Vite
+
+
+    
+
+
+
+
+
+
+
