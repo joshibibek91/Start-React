fcab4822332d5d303c20ac3878e9df6a0c6a2a1a
Bibek <132419097+joshibibek91@users.noreply.github.com>
2024-04-10 13:37:15 +0545

    Create createpages.jsx

---
+++ createpages.jsx
@@ -0,0 +1,9 @@
+// How to make other pages
+
+1. Make "pages" folder in existing "src" folder
+
+2. make Header and Footer folders with their corresponding jsx files and scss files in pages folder
+
+3. make rfce in both the jsx files and import scss from their stylesheets.
+
+4. 
