7884087b2a92e47ec353881ceed420df34a8733b
Bibek <132419097+joshibibek91@users.noreply.github.com>
2024-04-10 13:41:25 +0545

    Update createpages.jsx

--- createpages.jsx
+++ createpages.jsx
@@ -6,4 +6,33 @@
 
 3. make rfce in both the jsx files and import scss from their stylesheets.
 
-4. 
+4. Make Section.jsx file in src folder and make the routes of every pages. The Section file looks like the following
+
+
+import React from 'react';
+
+import { Routes, Route } from 'react-router-dom'
+
+import Home from './pages/Header/Home/Home';
+import Ourteam from './pages/Header/Team/Ourteam';
+import Aboutus from './pages/Header/Aboutus/Aboutus';
+import Services from './pages/Header/Services/Services';
+import Contact from './pages/Header/Contact/Contact';
+
+function Section() {
+    return (
+        <div style={{ display: "grid", gridTemplateRows: "auto 1fr auto" }}>
+
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/team" element={<Ourteam />} />
+                <Route path="/aboutus" element={<Aboutus />} />
+                <Route path="/services" element={<Services />} />
+                <Route path="/contact" element={<Contact />} />
+            </Routes>
+
+        </div>
+    )
+}
+
+export default Section
